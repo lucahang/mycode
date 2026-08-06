85310a3e9d6ba6e8d2af087616191a90bed8a2c1
lucahang <1547846906@qq.com>
2026-08-06 11:32:26 +0000

    add noncopyable.h

---
+++ noncopyable.h
@@ -0,0 +1,8 @@
+class NonCopyable{
+protected:
+    NonCopyable()=default;
+    ~NonCopyable()=default;
+public:
+    NonCopyable(const NonCopyable&)=delete;
+    NonCopyable operator=(const NonCopyable&)=delete;
+};
