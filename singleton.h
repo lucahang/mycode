5879c1605a0a2329b37bafebd22aefc3a1a993b8
lucahang <1547846906@qq.com>
2026-08-06 14:30:48 +0000

    add singleton.h

---
+++ singleton.h
@@ -0,0 +1,12 @@
+template<typename T>
+class Singleton{
+public:
+    static T& getInstance(){
+        static T instance;
+        return instance;
+    }
+    Singleton(const Singleton&) = delete;
+    Singleton& operator=(const Singleton&) = delete;
+protected:
+    Singleton() = default;
+};
